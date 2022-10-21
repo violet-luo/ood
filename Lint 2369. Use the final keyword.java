158ba054ba7e03344e6747dcd490cacea485d53d
Violet Luo <luo.violeta@gmail.com>
2022-10-21 11:37:10 -0700

    Create Lint 2369. Use the final keyword.java

---
+++ Lint 2369. Use the final keyword.java
@@ -0,0 +1,7 @@
+public class Solution {
+    private final String SENTENCES = "TO BE OR NOT TO BE";
+    
+    public final void test(String name) {
+        System.out.println(name + " said: " + this.SENTENCES);
+    }
+}
