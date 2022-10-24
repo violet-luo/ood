035c4a20f86dd2166c10efa2d10aac8caa02fc0b
Violet Luo <luo.violeta@gmail.com>
2022-10-24 16:09:41 -0700

    Create 2842. The constructor without parameters I.java

---
+++ 2842. The constructor without parameters I.java
@@ -0,0 +1,11 @@
+public class Student {
+    public String name;
+    public int age;
+    public double height;
+    
+    public Student() {
+        this.name = "Tom";
+        this.age = 20;
+        this.height = 180.0;
+    }
+}
